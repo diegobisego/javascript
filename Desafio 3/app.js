817eaf87ec666d31d724b253120ea75d63bbc341
Diego Bisego <diegobisego@gmail.com>
2022-08-08 23:15:45 -0300

    finalizado agregar productos, mensaje de exito y productos en la lista. Se intentara agregar un boton de remover productos

--- Desafio 3/app.js
+++ Desafio 3/app.js
@@ -53,75 +53,72 @@ class Producto {
 let productos = [];
 let contador = 0
 
-
   /************************** DOM ***************************** */
  
-
+//obtengo el boton de agregar producto
 const boton = document.querySelector('#btn-carga');
 
+
+//funcion click para agregar prodcuto
 boton.addEventListener('click', () => {
-    
+  
+  //evito el reinicio de la pagina
   const form = document.querySelector('#form');
   form.addEventListener('submit', (e) =>{
     e.preventDefault()
   })
-    
+  
+  //traigo por dom todos los textbox y select
   const tipoProducto = document.querySelector('#selTipo');
   const tipoTamPeso = document.querySelector('#selTamPeso');
   const precio = Number(document.querySelector('#inPrecio').value);
   const stock = Number(document.querySelector('#inStock').value);
 
+  //traigo los textos de los select
   const selTipoProducto = tipoProducto.options[tipoProducto.selectedIndex].text;
   const selTipoTamPeso = tipoTamPeso.options[tipoTamPeso.selectedIndex].text;
-  contador++
 
-  const nuevoProducto = new Producto(contador,selTipoProducto,selTipoTamPeso,precio,stock)
+  //contador de ID
+  contador++;
+
+  //creo y apencheo el producto al array
+  const nuevoProducto = new Producto(contador,selTipoProducto,selTipoTamPeso,precio,stock);
   productos.push(nuevoProducto);
 
-  const divLista = document.querySelector('#lista')
-  const temp = document.querySelector('#temp-productos')
-  const lsProduct = temp.content.querySelector('#tr-list')
+  //realizo el creado del template
+  const divLista = document.querySelector('#lista');
+  const temp = document.querySelector('#temp-productos');
+  const lsProduct = temp.content.querySelector('#tr-list');
 
   let prodClone
   
   productos.forEach((elem) => {
-      prodClone = lsProduct.cloneNode(lsProduct,true);
+      prodClone = lsProduct.cloneNode(lsProduct,true)
       prodClone.children[0].innerText = elem.tipo
       prodClone.children[1].innerText = elem.tamanio
-      prodClone.children[2].innerText = elem.precio
-      prodClone.children[3].innerText = elem.stock
-  
-      
-  })
+      prodClone.children[2].innerText = '$ ' + elem.precio
+      prodClone.children[3].innerText = elem.stock      
+  });
 
-  divLista.appendChild(prodClone)
+  divLista.appendChild(prodClone);
+
+  //reseteo el formulario
+  form.reset()
+
+  //muestro mensaje de exito de carga que se cierra en 2 segundos
+  mensajeCarga('Producto cargado con exito!', 'success')
+  setTimeout(() => div.parentNode.removeChild(div),2000) 
 
 });
 
-// let objeto = [{
-//     id:1,
-//     tipo: 'blsdadsadsdsadasdasdsadsaanco',
-//     tamanio: '34',
-//     precio: 324,
-//     stock: 4553
-// }]
-
-// const cambio = document.querySelector('#cambio-listar-productos')
+/************************** FUNCIONES ADICIONALES ***************************** */
 
 
-// cambio.addEventListener('click', () => {
-//     debugger
-//     const divLista = document.querySelector('#lista')
-//     const temp = document.querySelector('#temp-productos')
-//     const lsProduct = temp.content.querySelector('#tr-list')
-    
-//     objeto.forEach((elem) => {
-//         let prodClone = lsProduct.cloneNode(lsProduct,true);
-//         prodClone.children[0].innerText = elem.tipo
-//         prodClone.children[1].innerText = elem.tamanio
-//         prodClone.children[2].innerText = elem.precio
-//         prodClone.children[3].innerText = elem.stock
-    
-//         divLista.appendChild(prodClone)
-//     })
-// })
+//creacion y declaracion de funcion para mensaje de exito
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const div = document.createElement('div')
+
+const mensajeCarga = (mensaje, tipo) => {  
+  div.innerHTML = '<div class="alert alert-' + tipo + ' alert-dismissible" role="alert">' + mensaje + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+  alertPlaceholder.append(div)
+}
