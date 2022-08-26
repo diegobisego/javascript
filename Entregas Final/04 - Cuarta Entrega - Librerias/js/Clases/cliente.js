a7a807b9e30a64b3d031307ebd2aa3da7942936c
Diego Bisego <diegobisego@gmail.com>
2022-08-26 14:07:48 -0300

    se agrega clientes, se sigue trabajando en la logica

---
+++ Entregas Final/04 - Cuarta Entrega - Librerias/js/Clases/cliente.js
@@ -0,0 +1,12 @@
+class Ciente {
+    constructor(id,nombre,dniCuit,telefono,direccion,ciudad,email,formaPago){
+        this.id = id;
+        this.nombre = nombre;
+        this.dniCuit = dniCuit
+        this.telefono = telefono
+        this.direccion = direccion
+        this.ciudad = ciudad
+        this.email = email;
+        this.formaPago = formaPago;
+    }
+}
--- Entregas Final/04 - Cuarta Entrega - Librerias/js/ingresoVentas.js
+++ Entregas Final/04 - Cuarta Entrega - Librerias/js/ingresoVentas.js
@@ -1,8 +1,28 @@
-/************* SE CARGAN LOS PRODUCTOS ACTIVOS EN EL SISTEMA ******************/ 
+const form = document.querySelector('#form');
+form.addEventListener('submit', (e) =>{
+  e.preventDefault()
+});
 
+const formVentas = document.querySelector('#formVentas');
+formVentas.addEventListener('submit', (e) =>{
+  e.preventDefault()
+});
+
+/************* VARIABLES ******************/ 
 let productos = JSON.parse(localStorage.getItem('productos'));
-const idVtaProd = document.querySelector('#idVtaProd')
+//template
+const tVentas = document.querySelector('#tVentas'); //primero
+const tempDetalleVenta = document.querySelector('#tempDetalleVenta');//segundo
+const trTemp = tempDetalleVenta.content.querySelector('#trTemp');
 
+
+//ventas
+const idVtaProd = document.querySelector('#idVtaProd')
+const btnagregarListaVenta = document.querySelector('#btnagregarListaVenta');
+const precioUnitario = document.querySelector('#inPrecUni')
+const cantidad = document.querySelector('#inCantidadVenta')
+
+/************* SE CARGAN LOS PRODUCTOS ACTIVOS EN EL SISTEMA ******************/ 
 
 for (const i of productos) {
   const option = document.createElement('option');
@@ -14,21 +34,67 @@ for (const i of productos) {
 
 /************* PRECIO DEL PRODUCTO SEGUN LA SELECCION ******************/ 
 
-const precioUnitario = () => {
-
+idVtaProd.addEventListener('change', () => {
   const seleccion = idVtaProd.options[idVtaProd.selectedIndex].text;
-  const precioUnitario = document.querySelector('#inPrecUni')
   
   productos.find(element => {
       element.tipo == seleccion ? precioUnitario.value = element.precio : null
   });
 
-}
+})
 
-// const btnAgregarVenta = document.querySelector('btnAgregarVenta');
+/************* SE AGREGA VENTA A LA LISTA ******************/ 
 
-idVtaProd.addEventListener('change', () => {
-  precioUnitario();
+let listaProducto = [];
+let contador = 0;
+
+btnagregarListaVenta.addEventListener('click', () => {
+  const producto = idVtaProd.options[idVtaProd.selectedIndex].text;
+  const cant = Number(cantidad.value);
+  const precio = Number(precioUnitario.value);
+  contador++
+
+  const prodClone = trTemp.cloneNode(trTemp,true)
+  prodClone.children[0].innerText = contador
+  prodClone.children[1].innerText = producto
+  prodClone.children[2].innerText = cant
+  prodClone.children[3].innerText = precio
+
+  //evento para eliminar el producto
+  const nuevoProdcuto = new ProductoVenta(contador,producto,cant,precio)
+  listaProducto.push(nuevoProdcuto)
+
+  let btn = prodClone.querySelector("#btnEliminar");    
+
+    //evento para eliminar el producto
+  btn.addEventListener('click', () => {
+      //sweetAlert
+      Swal.fire({
+          title: '¿Desea eliminar el producto?',
+          showDenyButton: true,
+          showCancelButton: true,
+          confirmButtonText: 'Eliminar',
+          denyButtonText: `Volver`,
+        }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            const index = listaProducto.findIndex(item => item.id == contador);
+            console.log(index)
+            const parent = btn.parentNode.parentNode
+          
+            listaProducto.splice(index,1)
+            parent.parentNode.removeChild(parent)
+            Swal.fire('¡Producto Eliminado!', '', 'success')
+          } else if (result.isDenied) {
+            Swal.fire('No se elimino el producto', '', 'info')
+          }
+      })
+  })
+
+  tVentas.appendChild(prodClone)
+
+  console.log(nuevoProdcuto)
 
 })
+
 
